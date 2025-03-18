22857669cdede95d15f62d6322b11678d8fc53f9
suzhova <suzhova@uoguelph.ca>
2025-03-18 19:56:08 -0400

    cant get for loop to work with the mock tpc set

--- code/02-good-enough-tpc-fitting.R
+++ code/02-good-enough-tpc-fitting.R
@@ -99,63 +99,64 @@ treatment_list <- unique(mock_ouput$treatment)
 output_dir1 <- "lactin2-tpc" 
 if (!dir.exists(output_dir1)) dir.create(output_dir1)
 
-for (treatment in treatment_list) {
-  a.i <- a.gt %>%
-    filter(treatment == treatment) # Remove any unnecessary columns if applicable
-  
-  if (nrow(a.i) == 0) next  # Skip if no data for the combination
-  
-  # Initialize AICc storage and plot list
-  model.AICc <- data.frame(model = character(), AICc = numeric(), stringsAsFactors = FALSE)
-  nls.plot.list <- list()
-  
-  # Function to fit models
-  fit_model <- function(model_name, formula, iter, param_count) {
-    start_vals <- get_start_vals(a.gt$Temp, a.gt$r.gt, model_name = model_name) #change a/i/ to a/gt
+for (i in seq_along(treatment_list)) { 
+  tryCatch({
+    a.i <- a.gt %>%
+      filter(treatment == treatment_list[i])  
     
-    nls_fit <- try(nls_multstart(
-      formula,
-      data = a.gt,
-      iter = rep(4, param_count),
-      start_lower = start_vals - 10,
-      start_upper = start_vals + 10,
-      lower = get_lower_lims(a.gt$Temp, a.gt$r.gt, model_name = model_name),
-      upper = get_upper_lims(a.gt$Temp, a.gt$r.gt, model_name = model_name),
-      supp_errors = 'Y',
-      convergence_count = FALSE
-    ), silent = TRUE)
+    if (nrow(a.i) == 0) next
     
-    if (inherits(nls_fit, "try-error")) return(NULL)
+    model.AICc <- data.frame(model = character(), AICc = numeric(), stringsAsFactors = FALSE)
+    nls.plot.list <- list()
     
-    # Store AICc
-    model.AICc <<- rbind(model.AICc, data.frame(model = model_name, AICc = AICc(nls_fit)))
+    fit_model <- function(model_name, formula, iter, param_count) {
+      start_vals <- get_start_vals(a.i$Temp, a.i$r.gt, model_name = model_name)
+      
+      nls_fit <- try(nls_multstart(
+        formula,
+        data = a.i,
+        iter = rep(4, param_count),
+        start_lower = start_vals - 10,
+        start_upper = start_vals + 10,
+        lower = get_lower_lims(a.i$Temp, a.i$r.gt, model_name = model_name),
+        upper = get_upper_lims(a.i$Temp, a.i$r.gt, model_name = model_name),
+        supp_errors = 'Y',
+        convergence_count = FALSE
+      ), silent = TRUE)
+      
+      if (inherits(nls_fit, "try-error")) return(NULL)
+      
+      # Store AICc
+      model.AICc <<- rbind(model.AICc, data.frame(model = model_name, AICc = AICc(nls_fit)))
+      
+      # Generate predictions (Fix: Use a.i instead of a.gt)
+      preds <- data.frame(Temp = seq(min(a.i$Temp) - 2, max(a.i$Temp) + 2, length.out = 100))
+      preds <- broom::augment(nls_fit, newdata = preds)
+      
+      # Plot
+      plot <- ggplot(preds) + 
+        geom_point(aes(Temp, r.gt), data = a.i, size = 5) +  
+        geom_line(aes(Temp, .fitted), col = 'darkslateblue', linewidth = 6) + 
+        theme_classic(base_size = 20) + 
+        theme(
+          axis.title = element_text(size = 24),  
+          axis.text = element_text(size = 20) 
+        ) +
+        ggtitle(model_name) + 
+        ylim(-3, 5)
+      
+      return(plot)
+    }
     
-    # Generate predictions
-    preds <- data.frame(Temp = seq(min(a.gt$Temp - 2), max(a.gt$Temp + 2), length.out = 100))
-    preds <- broom::augment(nls_fit, newdata = preds)
+    # Fit each model
+    nls.plot.list[['Lactin2']] <- fit_model("lactin2_1995", r.gt ~ lactin2_1995(temp = Temp, a, b, tmax, delta_t), 4, 4)
     
-    # Plot
-    plot <- ggplot(preds) + 
-      geom_point(aes(Temp, r.gt), data = a.gt, size = 5) +  
-      geom_line(aes(Temp, .fitted), col = 'violet', linewidth = 6) + 
-      theme_classic(base_size = 20) + 
-      theme(
-        axis.title = element_text(size = 24),  
-        axis.text = element_text(size = 20) 
-      ) +
-      ggtitle(model_name) + 
-      ylim(-3, 5)
+    # Save plots (Fix: Use treatment_list[i] for filename)
+    mod_grid <- plot_grid(plotlist = nls.plot.list)
+    plot_filename <- paste0(output_dir1, "/TPC_plots_", treatment_list[i], ".pdf")
+    ggsave(plot_filename, plot = mod_grid, width = 24, height = 16)
     
-    
-    return(plot)
-  }
-  
-  # Fit each model
-  nls.plot.list[['Lactin2']] <- fit_model("lactin2_1995", r.gt ~ lactin2_1995(temp = Temp, a, b, tmax, delta_t), 4, 4)
-  
-  # Save plots
-  mod_grid <- plot_grid(plotlist = nls.plot.list)
-  plot_filename <- paste0(output_dir1, "/TPC_plots_", treatment, ".pdf")
-  ggsave(plot_filename, plot = mod_grid, width = 24, height = 16)
-}
+  }, error = function(e) {
+    message(paste("Error in iteration", i, ":", conditionMessage(e)))
+  })
 }
