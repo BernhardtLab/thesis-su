12c4cffbf8ab380a31a5898473cfe93ebc421831
suzhova <suzhova@uoguelph.ca>
2025-03-28 10:34:02 -0400

    48 has a significantly higher rmax than inf (p = 0.009, estimate = 0.0776).

--- code/03-fluctuation-period.R
+++ code/03-fluctuation-period.R
@@ -85,3 +85,44 @@ kruskal.test(t_breadth ~ period_fluctuation, data = out_nor_f5)
 oneway.test(t_breadth ~ period_fluctuation, data = out_nor_f5, var.equal = FALSE)
 #F = 2.2601, num df = 2.000, denom df = 17.182, p-value = 0.1345
 
+# r max -------------------------------------------------------------------
+#make df with unique values only
+out_nor_f5 <- out_nor_f5 %>% 
+  unite("trt", "incubator", "flask", sep = "_")
+output_norberg_f <- output_norberg_f %>% 
+  unite("trt", "incubator", "flask", sep = "_")
+
+out_nor_6 <- out_nor_f5 %>%
+  left_join(output_norberg_f %>% distinct(trt, .keep_all = TRUE), by = "trt") %>% 
+  rename("period_fluctuation" = "period_fluctuation.x")
+View(out_nor_6)
+
+#test for normality
+out_nor_6 %>%
+  filter(period_fluctuation == "6") %>% 
+  pull(rmax) %>%  
+  shapiro.test()#W = 0.90803, p-value = 0.3404 #matches line 236 of 03-stat-comaprsions
+out_nor_6 %>%
+  filter(period_fluctuation == "48")%>% 
+  pull(rmax) %>%  
+  shapiro.test() #W = 0.89391, p-value = 0.2544 #matches
+out_nor_6 %>%
+  filter(period_fluctuation == "inf")%>% 
+  pull(rmax) %>%  
+  shapiro.test() #W = 0.91591, p-value = 0.1449
+
+leveneTest(rmax ~ period_fluctuation, data = out_nor_6) #p = 0.001336 
+
+#KW
+kruskal.test(rmax ~ period_fluctuation, data = out_nor_6)
+#Kruskal-Wallis chi-squared = 6.3203, df = 2, p-value = 0.04242
+
+#Welch ANOVA
+oneway.test(rmax ~ period_fluctuation, data = out_nor_6, var.equal = FALSE)
+#F = 5.4761, num df = 2.000, denom df = 18.912, p-value = 0.0133
+
+#post hoc for Welch: Games Howell
+rmax_games_howell_results <- out_nor_6 %>%
+  games_howell_test(rmax ~ period_fluctuation)
+View(rmax_games_howell_results)
+#48 has a significantly higher rmax than inf (p = 0.009, estimate = 0.0776)
