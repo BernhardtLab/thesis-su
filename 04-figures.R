fa5a566363c9c9047143df164008f5181386a716
suzhova <suzhova@uoguelph.ca>
2025-03-27 21:56:25 -0400

    t max, need to fix t opt

--- 04-figures.R
+++ 04-figures.R
@@ -237,3 +237,77 @@ tradeoff_df %>%
     y = "T breadth",
     colour = "Treatment")
 
+
+# T opt -------------------------------------------------------------------
+
+
+# T max -------------------------------------------------------------------
+unique_tmax_df %>% 
+  ggplot(aes(x = incubator, y = tmax, fill = incubator)) + 
+  # Create a boxplot
+  geom_boxplot(alpha = 0.6, outlier.shape = NA) +  
+  # Add scatter points for better visualization
+  geom_jitter(position = position_jitterdodge(jitter.width = 0), alpha = 0.5) +  
+  # Since ANOVA showed no significant differences, we do not add significance stars
+  # Add a basic theme and labels
+  theme_minimal() +
+  labs(
+    x = "Treatment",
+    y = "T max",
+    fill = "Treatment"
+  ) +
+  scale_fill_manual(values = c("14C" = "#145da0", "30C" = "#bc1823", "6F" = "#ff8210", "48F" = "#800080"))  # Customize colors
+
+# fluctuation period ---------------------------------------------------------
+#copying over the df making#####################################################
+output_norberg_f <- output_norberg2 %>%
+  # Separate the 'incubator' column into 'num' and 'chr'
+  mutate(
+    num = substr(incubator, 1, nchar(incubator) - 1),  # Everything but the last character
+    chr = substr(incubator, nchar(incubator), nchar(incubator))  # Last character
+  ) %>%
+  # Create the 'period_fluctuation' column based on 'chr'
+  mutate(
+    period_fluctuation = case_when(
+      chr == "C" ~ "inf",  # If 'chr' is "C", set "inf"
+      chr == "F" ~ as.character(num),  # If 'chr' is "F", set 'num' value
+      TRUE ~ NA_character_  # For other cases (optional), set NA or other default
+    )
+  )
+# bring in T breadth
+output_norberg_f3 <- output_norberg_f %>% 
+  filter(predicted_growth >= 0.5 * rmax) %>% 
+  unite("trt", "incubator", "flask", sep = "_") %>% 
+  group_by(trt) %>%
+  summarise(t_breadth = max(temp) - min(temp), .groups = "drop") %>% 
+  separate(col = trt, into = c("incubator", "flask"), sep = "_")
+
+output_norberg_f4 <- left_join(output_norberg_f, output_norberg_f3)
+View(output_norberg_f4)
+
+output_norberg_f4 %>% 
+  ggplot(aes(x = period_fluctuation, y = t_breadth, colour = incubator)) + 
+  geom_point() + 
+  theme_minimal() + 
+  labs(
+    x = "Fluctuation period (hours)",
+    y = "T breadth",
+    colour = "Treatment")
+
+output_norberg_f4 %>% 
+  ggplot(aes(x = period_fluctuation, y = rmax, colour = incubator)) + 
+  geom_point() + 
+  theme_minimal() + 
+  labs(
+    x = "Fluctuation period (hours)",
+    y = "r max",
+    colour = "Treatment")
+
+output_norberg_f4 %>% 
+  ggplot(aes(x = period_fluctuation, y = topt, colour = incubator)) + 
+  geom_point() + 
+  theme_minimal() + 
+  labs(
+    x = "Fluctuation period (hours)",
+    y = "T opt",
+    colour = "Treatment")
